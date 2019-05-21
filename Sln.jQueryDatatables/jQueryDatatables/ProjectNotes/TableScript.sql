c54d53a6945e7e4897345b19b16f093d9fd2c456
shahedbd <shahed.mbstu@gmail.com>
2019-05-21 12:39:39 +0600

    + Script update + Doc update

--- Sln.jQueryDatatables/jQueryDatatables/ProjectNotes/TableScript.sql
+++ Sln.jQueryDatatables/jQueryDatatables/ProjectNotes/TableScript.sql
@@ -28,3 +28,22 @@ CREATE TABLE [dbo].[PersonalInfo](
 
 GO
 
+
+
+
+--Data Insertion
+----truncate <br />
+truncate table PersonalInfo
+---SQL loop insert 
+DECLARE @ID int =0; 
+DECLARE @StartDate AS DATETIME = '1980-01-01' 
+WHILE @ID < 20
+BEGIN 
+insert into PersonalInfo values('First Name ' + CAST(@ID AS nvarchar),'Last Name ' + CAST(@ID AS VARCHAR),dateadd(day,1, @StartDate), 
+'City ' + CAST(@ID AS VARCHAR),'Country ' + CAST(@ID AS VARCHAR),ABS(CAST(NEWID() AS binary(12)) % 1000) + 5555, 
+ABS(CAST(NEWID() AS binary(12)) % 1000) + 99998888,'email' + CAST(@ID AS nvarchar) +'@gmail.com',
+GETDATE(),null,'Admin' + CAST(@ID AS VARCHAR),null,1) 
+SET @ID = @ID + 1; 
+set @StartDate=dateadd(day,1, @StartDate) 
+END
+
